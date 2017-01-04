26ad8b0666b962f96a131e8bd6d80cf9b581f655
kunci115 <rino.alpin@gmail.com>
2017-01-04 13:00:34 +0700

    Add files via upload

---
+++ algoritma/swapcase.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  6 11:18:27 2016
+
+@author: root
+"""
+
+print(raw_input().swapcase())
---
+++ algoritma/tuples.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 15 20:51:49 2016
+
+@author: root
+"""
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    integer_list = map(int, raw_input().split())
+print integer_list
